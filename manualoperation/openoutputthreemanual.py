087668da4cb2d7c96e7b184fb676586719b6a2e9
thegroundhogwhisperer <45322024+thegroundhogwhisperer@users.noreply.github.com>
2018-11-25 00:28:50 -0500

    Add files via upload

---
+++ manualoperation/openoutputthreemanual.py
@@ -0,0 +1,14 @@
+# manual operation output two on
+import time
+import automationhat
+time.sleep(0.1) # short pause after ads1015 class creation recommended
+import subprocess
+
+print("Output two manual on operation starting.")
+
+pigsGPIOCommandLine = ["/usr/bin/pigs", "w 6 1"]
+p = subprocess.Popen(pigsGPIOCommandLine)
+
+print("Output two manual on operation starting.")
+
+
