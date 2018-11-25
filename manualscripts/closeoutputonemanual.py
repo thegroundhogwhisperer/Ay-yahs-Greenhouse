0276d05755f006228f92cc6146552a4ead500b47
thegroundhogwhisperer <45322024+thegroundhogwhisperer@users.noreply.github.com>
2018-11-25 00:22:11 -0500

    Add files via upload

---
+++ manualscripts/closeoutputonemanual.py
@@ -0,0 +1,14 @@
+# manual operation output one off
+import time
+import automationhat
+time.sleep(0.1) # short pause after ads1015 class creation recommended
+import subprocess
+
+print("Output one manual off operation starting.")
+
+pigsGPIOCommandLine = ["/usr/bin/pigs", "w 5 0"]
+p = subprocess.Popen(pigsGPIOCommandLine)
+
+print("Output one manual off operation starting.")
+
+
---
+++ manualscripts/closeoutputthreemanual.py
@@ -0,0 +1,14 @@
+# manual operation output two off
+import time
+import automationhat
+time.sleep(0.1) # short pause after ads1015 class creation recommended
+import subprocess
+
+print("Output two manual off operation starting.")
+
+pigsGPIOCommandLine = ["/usr/bin/pigs", "w 6 0"]
+p = subprocess.Popen(pigsGPIOCommandLine)
+
+print("Output two manual off operation starting.")
+
+
