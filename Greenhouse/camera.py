1f3d400bf743d74758789c62efafc08dac6ba989
The Groundhog Whisperer <45322024+thegroundhogwhisperer@users.noreply.github.com>
2019-07-28 15:15:23 -0400

    Version 1.04

--- Greenhouse/camera.py
+++ Greenhouse/camera.py
@@ -6,7 +6,7 @@
 ## Description: A component of Ay-yahs-Greenhouse Automation System ##
 ## Description: Reads luminosity sensor and produces image data     ##
 ## Description: from a camera.					    ##
-## Version: 1.03					 	    ##
+## Version: 1.04					 	    ##
 ## Project Repository: https://git.io/fhhsY			    ##
 ## Copyright (C) 2019 The Groundhog Whisperer			    ##
 ######################################################################
