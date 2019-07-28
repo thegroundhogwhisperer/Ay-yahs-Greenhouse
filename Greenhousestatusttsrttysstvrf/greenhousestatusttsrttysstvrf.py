d9c28053604dd575a4e91db264ec2f823d04b3ee
The Groundhog Whisperer <45322024+thegroundhogwhisperer@users.noreply.github.com>
2019-07-28 15:21:16 -0400

    Version 1.04

--- Greenhousestatusttsrttysstvrf/greenhousestatusttsrttysstvrf.py
+++ Greenhousestatusttsrttysstvrf/greenhousestatusttsrttysstvrf.py
@@ -6,7 +6,7 @@
 ## Description: A component of Ay-yahs-Greenhouse Automation System ##
 ## Description: Produces GUI notification, text-to-speech, RTTY,    ##
 ## Description: SSTV audio for radio frequency transmission	    ##
-## Version: 1.03						    ##
+## Version: 1.04						    ##
 ## Project Repository: https://git.io/fhhsY			    ##
 ## Copyright (C) 2019 The Groundhog Whisperer			    ##
 ######################################################################
